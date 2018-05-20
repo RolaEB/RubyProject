ac15a9c787516a7677e3c827b5b1219853b4a51f
rola <rola.elbakly@gmail.com>
2018-05-20 23:20:55 +0200

    model tests

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -4,4 +4,17 @@ class CourseTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'valid course' do
+    course = Course.new(title: 'PHP', user_id: 3)
+    assert course.valid?
+  end
+
+  test 'invalid without title' do
+    course = Course.new(user_id: 3)
+    refute course.valid?, 'course is valid without a title'
+    assert_not_nil course.errors[:title], 'no validation error for title present'
+  end
+
+  
 end
